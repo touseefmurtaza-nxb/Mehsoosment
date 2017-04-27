ba4348eae6da374423d95a37233ad0949bc66def
Touseef Murtaza <touseef.murtaza@nxb.com.pk>
2017-04-27 19:29:13 +0500

    north, south, east, west coordinates search added for geocoder

--- app/controllers/api/v1/user_locations_controller.rb
+++ app/controllers/api/v1/user_locations_controller.rb
@@ -8,12 +8,17 @@ module Api
       param :latitude, Float, desc: 'User current location latitude', required: true
       param :longitude, Float, desc: 'User current location longitude', required: true
       param :user_id, Integer, desc: 'User id, whose current location is marked', required: true
+      param :ne, Integer, desc: 'North East', required: true
+      param :se, Integer, desc: 'South East', required: true
+      param :nw, Integer, desc: 'North West', required: true
+      param :sw, Integer, desc: 'South West', required: true
       def create
         @location = UserLocation.new(latitude: params[:latitude], longitude: params[:longitude], user_id: params[:user_id])
         if @location.save
-          @markups = MarkFeeling.near([@location.latitude,@location.longitude],6)
+          # @markups = MarkFeeling.near([@location.latitude,@location.longitude],6)
+          @markups = MarkFeeling.where(:latitude => params[:ne]..params[:se], :longitude => params[:nw]..params[:sw])
           @stats = @markups.group_by(&:mark_type).map {|k,v| [MarkFeeling::MARK_TYPE[k], v.length]}
-          send_notification(@markups)
+          # send_notification(@markups)
           if @markups
             render :json => {
                        success: "true",
