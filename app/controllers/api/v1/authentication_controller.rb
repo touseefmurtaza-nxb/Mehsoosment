37fbd9dd97a0cf4ca069d76017090d8ad92c229e
Touseef Murtaza <touseef.murtaza@nxb.com.pk>
2017-06-13 15:53:01 +0500

    authentication_controller: added to api doc

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -3,6 +3,11 @@ module Api
 		class AuthenticationController < ApplicationController
 			skip_before_action :authenticate_request
 
+			# ---------------------------------------- Mark Danger Location Point --------------------------------------------
+      api :POST, '/v1/users/authenticate', 'Authenticate User'
+      param :email, String, desc: 'User Email', required: true
+      param :password, String, desc: 'User password', required: true
+
 			def authenticate
 				command = AuthenticateUser.call(params[:email], params[:password])
 
