64042079f7ffb062a25a83c441444107efa339a1
Touseef Murtaza <touseef.murtaza@nxb.com.pk>
2017-07-04 14:20:30 +0500

    rooms_controller: leave room api added

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -6,6 +6,7 @@ module Api
       # ---------------------------------------- Set Messages Seen ---------------------------------------------------
       api :POST, '/v1/rooms/read_messages', 'Set Room Messages to Seen'
       param :id, Integer, desc: 'Room id',required: true
+      param :user_id, Integer, desc: 'User who tap on chat room',required: true
 
       example <<-EOS
       {
@@ -20,7 +21,7 @@ module Api
          Authentication token has to be passed as part of the request. It can be passed as parameter in HTTP header(Authorization).
       EOS
       def read_messages
-        @room.messages.where(seen: false).update_all(seen: true)
+        @room.messages.where(seen: false, receiver_id: params[:user_id]).update_all(seen: true)
         render json: {success: true, messgae: "Messages Seen", data: "", status: 200}
       end
 
@@ -43,8 +44,31 @@ module Api
          Authentication token has to be passed as part of the request. It can be passed as parameter in HTTP header(Authorization).
       EOS
       def unseen_messages
-        unseen_msgs = @room.messages.where(seen: false)
+        unseen_msgs = @room.messages.where(seen: false, receiver_id: params[:user_id])
         render json: {success: true, messgae: "", data: {unseen_msgs_count: unseen_msgs.count}.as_json, status: 200}
+      end
+
+      # ---------------------------------------- Reset Message Counter While Leaving Room ---------------------------------------------------
+      api :POST, '/v1/rooms/leave_room', 'Reset Message Counter While Leaving Room'
+      param :id, Integer, desc: 'Room id',required: true
+      param :user_id, Integer, desc: 'User who leave the chat room',required: true
+
+      example <<-EOS
+      {
+        "success": true,
+        "messgae": "Messages Counter Reset",
+        "data": "",
+        "status": 200
+      }
+      EOS
+      description <<-EOS
+        == Authentication required
+         Authentication token has to be passed as part of the request. It can be passed as parameter in HTTP header(Authorization).
+      EOS
+      def leave_room
+        @room.messages.where(seen: false, receiver_id: params[:user_id]).update_all(seen: true)
+        # @room.messages.where("messages.seen = ? AND (messages.receiver_id = ? OR messages.sender_id = ?)", false, params[:user_id], params[:user_id]).update_all(seen: true)
+        render json: {success: true, messgae: "Messages Counter Reset", data: "", status: 200}
       end
 
       private
